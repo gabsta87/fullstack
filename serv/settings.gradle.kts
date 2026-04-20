3a1e67af71f8f79c5045721e3ee6976cded7757d
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2026-04-20 16:07:47 +0200

    gradle files corrected

--- serv/settings.gradle.kts
+++ serv/settings.gradle.kts
@@ -2,6 +2,7 @@ pluginManagement {
     repositories {
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("https://repo.spring.io/release") }
     }
 }
 rootProject.name = "serv"
