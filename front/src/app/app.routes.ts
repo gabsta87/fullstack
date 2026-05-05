41ee89e4f0a5770666ad77269e158ce549430151
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2026-05-05 19:27:29 +0200

    route for profile-management added

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -7,16 +7,17 @@ import {LoginComponent} from "./features/components/login/login.component";
 import {AccountComponent} from "./features/components/account/account.component";
 import {authGuard} from "./features/guards/auth.guard";
 import {galleryResolver} from "./features/resolver/gallery.resolver";
+import {ProfileManagementComponent} from "./features/components/profile-management/profile-management.component";
 
 export const routes: Routes = [
   // Gallery resolver loads WorkerGalleryDTOs (with mainThumbUrl) for the homepage
   { path: '',        component: HomepageComponent, resolve: { workers: galleryResolver } },
   { path: 'home',    component: HomepageComponent, resolve: { workers: galleryResolver } },
   { path: 'gallery', component: HomepageComponent, resolve: { workers: galleryResolver } },
-
-  { path: 'login',            component: LoginComponent },
-  { path: 'ngx-pay',          component: NgxPayComponent },
-  { path: 'account',          component: AccountComponent, canActivate: [authGuard] },
+  { path: 'profile-management', component: ProfileManagementComponent },
+  { path: 'login',              component: LoginComponent },
+  { path: 'ngx-pay',            component: NgxPayComponent },
+  { path: 'account',            component: AccountComponent, canActivate: [authGuard] },
 
   // Profile uses UUID from queryParam ?id=
   { path: 'profile', component: ProfileComponent, resolve: { profile: detailedProfileResolver } },
