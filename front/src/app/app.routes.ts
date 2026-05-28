62e946a583b9ba1dda418b74f9ab42f95c6005c7
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2026-05-28 18:04:46 +0200

    front : route corrected

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -16,7 +16,7 @@ export const routes: Routes = [
   { path: '',        component: HomepageComponent, resolve: { workers: galleryResolver, allServices : servicesResolver} },
   { path: 'home',    component: HomepageComponent, resolve: { workers: galleryResolver, allServices : servicesResolver } },
   { path: 'gallery', component: HomepageComponent, resolve: { workers: galleryResolver, allServices : servicesResolver } },
-  { path: 'profile', component: ProfileComponent,  resolve: { profile: profileVisitingResolver }, canActivate: [authGuard] },
+  { path: 'profile', component: ProfileComponent,  resolve: { profile: profileVisitingResolver } },
   { path: 'ngx-pay', component: NgxPayComponent, canActivate: [authGuard] },
   { path: 'account', component: AccountComponent, resolve: { profile: accountResolver }, canActivate: [authGuard] },
   { path: 'profile-management', component: ProfileManagementComponent, resolve: { profile: profileManagementResolver, services : servicesResolver }, canActivate: [authGuard, workerOnlyGuard] },
