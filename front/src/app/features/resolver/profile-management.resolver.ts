7ea666cbe083753c8ae2d43fc655a7b0f344023c
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2026-06-01 17:43:45 +0200

    front : redirection removed from profile-management resolver

--- front/src/app/features/resolver/profile-management.resolver.ts
+++ front/src/app/features/resolver/profile-management.resolver.ts
@@ -1,19 +1,11 @@
-import {ResolveFn, Router} from '@angular/router';
+import {ResolveFn} from '@angular/router';
 import {inject} from "@angular/core";
-import {EMPTY, map, Observable} from "rxjs";
+import {Observable} from "rxjs";
 import {WorkerPrivateAccount} from "../models/user.model";
 import {WorkerAccountService} from "../services/worker-account.service";
 
 export const profileManagementResolver: ResolveFn<Observable<WorkerPrivateAccount>> = (route, state) => {
   const userService = inject(WorkerAccountService);
-  const router = inject(Router);
 
-  const user = userService.getCurrentAccount();
-
-  if (user?.pipe(map(e => {e.role === 'CLIENT'}))) {
-    router.navigate(['/account']);
-    return EMPTY;
-  }
-
-  return user;
+  return userService.getCurrentAccount();
 };
