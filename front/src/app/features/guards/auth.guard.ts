4337f2ee9de6c73387e4b9d548aa204594490ba0
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2026-06-08 19:43:39 +0200

    front : stronger session-check

--- front/src/app/features/guards/auth.guard.ts
+++ front/src/app/features/guards/auth.guard.ts
@@ -14,19 +14,10 @@ export const authGuard: CanActivateFn = (route, state) => {
 
   // 2. "Slow Path" : Appel serveur pour vérifier la session
   return authService.checkSession().pipe(
-    map(isAuth => {
-      if (isAuth) return true;
-
-      // Si pas authentifié, on mémorise l'URL et on redirige
-      authService.setRedirectUrl(state.url);
-      router.navigate(['/login']);
-      return false;
+    map(() => {
+      return true;
     }),
     catchError((err) => {
-      // 3. Gestion d'erreur (réseau, timeout, 500...)
-      // On considère une erreur comme "non authentifié"
-      console.error("authGuard: Erreur lors de la vérification de session", err);
-
       authService.setRedirectUrl(state.url);
       router.navigate(['/login']);
       return of(false);
