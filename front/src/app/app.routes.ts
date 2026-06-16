2e3f10525bac84a684c59dee1daf7470c6225aaa
Gabriel Maret <gabrielmaretsecondaire@gmail.com>
2026-06-16 11:22:29 +0200

    front : updated routing for homepage

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,5 +1,5 @@
-import { Routes } from '@angular/router';
-import { HomepageComponent } from './features/components/homepage/homepage.component';
+import {Routes} from '@angular/router';
+import {HomepageComponent} from './features/components/homepage/homepage.component';
 import {NgxPayComponent} from "./features/components/ngx-pay/ngx-pay.component";
 import {ProfileComponent} from "./features/components/profile/profile.component";
 import {AccountComponent} from "./features/components/account/account.component";
@@ -11,15 +11,39 @@ import {profileManagementResolver} from "./features/resolver/profile-management.
 import {accountResolver} from "./features/resolver/account.resolver";
 import {workerOnlyGuard} from "./features/guards/worker-only.guard";
 import {profileVisitingResolver} from "./features/resolver/profile-visiting.resolver";
+import {geographicZonesResolver} from "./features/resolver/geographic-zones.resolver";
 
 export const routes: Routes = [
-  { path: '',        component: HomepageComponent, resolve: { workers: galleryResolver, allServices : servicesResolver} },
-  { path: 'home',    component: HomepageComponent, resolve: { workers: galleryResolver, allServices : servicesResolver } },
-  { path: 'gallery', component: HomepageComponent, resolve: { workers: galleryResolver, allServices : servicesResolver } },
-  { path: 'profile', component: ProfileComponent,  resolve: { profile: profileVisitingResolver } },
+  {
+    path: '',
+    resolve: {
+      workers: galleryResolver,
+      allServices: servicesResolver,
+      locations: geographicZonesResolver
+    },
+    children: [
+      { path: 'home', component: HomepageComponent },
+      { path: 'gallery', component: HomepageComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' }
+    ]
+  },
+  { path: 'profile', component: ProfileComponent,
+    resolve: {
+      profile: profileVisitingResolver,
+      locations:    geographicZonesResolver,
+  } },
   { path: 'ngx-pay', component: NgxPayComponent, canActivate: [authGuard] },
-  { path: 'account', component: AccountComponent, resolve: { profile: accountResolver }, canActivate: [authGuard] },
-  { path: 'profile-management', component: ProfileManagementComponent, resolve: { profile: profileManagementResolver, services : servicesResolver }, canActivate: [authGuard, workerOnlyGuard] },
+  { path: 'account', component: AccountComponent, canActivate: [authGuard],
+    resolve: {
+      profile: accountResolver,
+      locations:    geographicZonesResolver,
+    } },
+  { path: 'profile-management', component: ProfileManagementComponent,
+    resolve: {
+      profile: profileManagementResolver,
+      services : servicesResolver,
+      locations: geographicZonesResolver,
+    }, canActivate: [authGuard, workerOnlyGuard] },
 
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
